067f143a2b4decd570d261b98b6b9127e6bde56a
ramazan351 <31069732+ramazan351@users.noreply.github.com>
2023-07-12 15:09:18 +0300

    tag filter added

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+    public function scopeFilter($query, array $filters)
+    {
+      // dd($filters);
+        $query->when($filters['tag'] ?? false, function ($query, $search) {
+            return $query->where('tags', 'like', '%' . $search . '%');
+        });
+    }
+
 }
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -9,7 +9,8 @@
 --}}
 
 <x-layout>
-
+    @include('partials._hero')
+    @include('partials._search')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
         @if (count($listings) == 0)
