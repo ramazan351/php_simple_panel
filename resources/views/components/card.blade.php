7cc1b47b1c685cebdd8f88ca762d229487586359
ramazan351 <31069732+ramazan351@users.noreply.github.com>
2023-07-12 13:07:40 +0300

    override properties

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,4 @@
+{{-- @dd($attributes) --}}
 <div {{ $attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded p-6']) }}>
     {{ $slot }}
 </div>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,7 +3,7 @@
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
-        <x-card>
+        <x-card class="!bg-black p-100">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
